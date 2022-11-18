177f2ef39e0e3e31e8506436a9a66c172f8e0aa1
AsanoSogen <sogen0921@icloud.com>
2022-11-18 22:50:22 +0900

    “first”

--- lib/cdk_test-stack.ts
+++ lib/cdk_test-stack.ts
@@ -1,16 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as rds from 'aws-cdk-lib/aws-rds';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'Vpc');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkTestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const postgres = new rds.DatabaseInstance(this, 'Postgres', {
+        engine: rds.DatabaseInstanceEngine.postgres({
+          version: rds.PostgresEngineVersion.VER_14_2, // バージョン指定
+        }),
+        instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MEDIUM),
+        vpc:vpc,
+        vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC }
+    })
+
+    postgres.connections.allowFromAnyIpv4(ec2.Port.tcp(5432))
   }
 }
