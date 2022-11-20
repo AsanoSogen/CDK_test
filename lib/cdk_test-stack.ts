07d3e813f85e3fef9b6bfa3e8db401dd1680eec5
AsanoSogen <sogen0921@icloud.com>
2022-11-21 01:43:57 +0900

    createRDB

--- lib/cdk_test-stack.ts
+++ lib/cdk_test-stack.ts
@@ -2,7 +2,6 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as rds from 'aws-cdk-lib/aws-rds';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -10,13 +9,16 @@ export class CdkTestStack extends cdk.Stack {
 
     const vpc = new ec2.Vpc(this, 'Vpc');
 
+    const rdsCredentials: rds.Credentials =  rds.Credentials.fromGeneratedSecret("gorm");
     const postgres = new rds.DatabaseInstance(this, 'Postgres', {
         engine: rds.DatabaseInstanceEngine.postgres({
           version: rds.PostgresEngineVersion.VER_14_2, // バージョン指定
         }),
-        instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MEDIUM),
+        instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MICRO),
         vpc:vpc,
-        vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC }
+        vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+        databaseName: "dbName",
+        credentials: rdsCredentials
     })
 
     postgres.connections.allowFromAnyIpv4(ec2.Port.tcp(5432))
